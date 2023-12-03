f770a81f60727adf1493d30d6b0915ba39c17536
Kayshav <74193605+Kayshavr@users.noreply.github.com>
2023-12-03 18:19:30 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 
+echo "Hello Azure1!";
+$serverHost = "iot-health-app.database.windows.net";
+$user = "iot-health-app";
+$password = "fsktm123$";
+$databaseName = "iothealthapp-database";
+
+$connectNow = new mysqli_connect($serverHost, $user, $password, $databaseName);
+
+// Check connection
+if ($connectNow->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 echo "Hello Azure2!";
