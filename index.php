63a412e74ce76a61c9f4379b3f1552024a831a06
Kayshav <74193605+Kayshavr@users.noreply.github.com>
2023-12-03 18:33:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,17 @@
 <?php
-
-echo "Hello Azure5!";
-$servername = "iot-health-app.database.windows.net";
-$username = "iot-health-app";
-$password = "fsktm123$";
-$databaseName = "iothealthapp-database";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:iot-health-app.database.windows.net,1433; Database = iot-health-app", "iot-health-app", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-echo "Connected successfully";
-?>
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
 
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "iot-health-app", "pwd" => "{your_password_here}", "Database" => "iot-health-app", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:iot-health-app.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+echo "Connected";
+?>
