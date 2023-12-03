30d00b20df1f2ef5b5fd65d5a89daff00396836f
Kayshav <74193605+Kayshavr@users.noreply.github.com>
2023-12-03 18:35:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:iot-health-app.database.windows.net,1433; Database = iot-health-app", "iot-health-app", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:iot-health-app.database.windows.net,1433; Database = iot-health-app", "iot-health-app", "fsktm123$");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "iot-health-app", "pwd" => "{your_password_here}", "Database" => "iot-health-app", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "iot-health-app", "pwd" => "fsktm123$", "Database" => "iot-health-app", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:iot-health-app.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 echo "Connected";
